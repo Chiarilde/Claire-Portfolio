076d3fc93f20c2b39ad0c20792e15a1c2c35f70c
Chiarilde <chiarilde87@gmail.com>
2025-04-11 21:28:40 +0200

    index-it added

---
+++ script.js
@@ -0,0 +1,7 @@
+function cambiaLingua(lang) {
+    if (lang === "it") {
+        window.location.href = "index-it.html";
+    } else if (lang === "en") {
+        window.location.href = "index.html";
+    }
+}
