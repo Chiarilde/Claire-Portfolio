938ba90adec689455b57c0d79b46484b4e084eed
Chiarilde <chiarilde87@gmail.com>
2025-04-13 18:21:34 +0200

    responsive

--- script.js
+++
@@ -1,7 +0,0 @@
-function cambiaLingua(lang) {
-    if (lang === "it") {
-        window.location.href = "index-it.html";
-    } else if (lang === "en") {
-        window.location.href = "index.html";
-    }
-}
